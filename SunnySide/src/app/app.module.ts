f757341aad0240a81ffcdf16f3a5c787cebf8d08
jormazlop <jormazlop@gmail.com>
2022-06-18 11:21:59 +0200

    SunnySide

---
+++ SunnySide/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FirstSectionComponent } from './components/first-section/first-section.component';
+import { HeaderComponent } from './components/first-section/header/header.component';
+import { SecondSectionComponent } from './components/second-section/second-section.component';
+import { ThirdSectionComponent } from './components/third-section/third-section.component';
+import { FourthSectionComponent } from './components/fourth-section/fourth-section.component';
+import { FifthSectionComponent } from './components/fifth-section/fifth-section.component';
+import { SixthSectionComponent } from './components/sixth-section/sixth-section.component';
+import { SeventhSectionComponent } from './components/seventh-section/seventh-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstSectionComponent,
+    HeaderComponent,
+    SecondSectionComponent,
+    ThirdSectionComponent,
+    FourthSectionComponent,
+    FifthSectionComponent,
+    SixthSectionComponent,
+    SeventhSectionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
