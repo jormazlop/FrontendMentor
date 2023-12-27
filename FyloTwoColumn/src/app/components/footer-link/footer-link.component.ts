3d3a5611043860c2d11ea400d04d3b5784fb772e
jormazlop <jormazlop@gmail.com>
2023-12-27 08:38:58 +0100

    FyloTwoColumn

---
+++ FyloTwoColumn/src/app/components/footer-link/footer-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-link.component.html',
+  styleUrl: './footer-link.component.scss'
+})
+export class FooterLinkComponent {
+
+}
--- FyloTwoColumn/src/app/sections/footer/footer.component.ts
+++ FyloTwoColumn/src/app/sections/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FacebookIconComponent } from '../../components/facebook-icon/facebook-icon.component';
+import { TwitterIconComponent } from '../../components/twitter-icon/twitter-icon.component';
+import { InstagramIconComponent } from '../../components/instagram-icon/instagram-icon.component';
+import { FooterLinkComponent } from '../../components/footer-link/footer-link.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FacebookIconComponent],
+  imports: [FacebookIconComponent, TwitterIconComponent, InstagramIconComponent, FooterLinkComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
