08cdf44b912aa80197d45119c519b32a105d4706
jormazlop <jormazlop@gmail.com>
2025-12-23 17:25:42 +0100

    FlashCardApp

--- FlashCardApp/src/app/flashcard-all/flashcard-list/flashcard-item/flashcard-item.ts
+++ FlashCardApp/src/app/flashcard-all/flashcard-list/flashcard-item/flashcard-item.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { MasteredTag } from '@shared/mastered-tag/mastered-tag';
 import { Tag } from '@shared/tag/tag';
 import { FlashcardModel } from 'model/flashcard.model';
 
 @Component({
   selector: 'flashcard-item',
-  imports: [Tag],
+  imports: [Tag, MasteredTag],
   templateUrl: './flashcard-item.html',
   styleUrl: './flashcard-item.scss',
 })
---
+++ FlashCardApp/src/app/shared/mastered-tag/mastered-tag.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IconMastered } from '@shared/icons/icon-mastered/icon-mastered';
+
+@Component({
+  selector: 'mastered-tag',
+  imports: [IconMastered],
+  templateUrl: './mastered-tag.html',
+  styleUrl: './mastered-tag.scss',
+})
+export class MasteredTag {
+
+}
